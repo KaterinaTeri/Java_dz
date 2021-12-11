bc79c186fb31e19017373865e9feed021930aa55
KaterinaTeri <95863911+KaterinaTeri@users.noreply.github.com>
2021-12-11 11:26:10 +0300

    Update PervoeDZ.java

--- PervoeDZ.java
+++ PervoeDZ.java
@@ -1,9 +1,9 @@
 class PervoeDZ {
     public static void main(String[] args) {
         printThreeWords();
-	checkSumSign();
-	printColor();
-	compareNumbers();
+        checkSumSign();
+        printColor();
+        compareNumbers();
     }
 	
     static void printThreeWords() {
@@ -13,25 +13,25 @@ class PervoeDZ {
     }
 	
     static void checkSumSign() {
-	int a = -10;
-	int b = 7;
-	System.out.println(a+b>=0? "The amount is positive" : "The amount is negative");
+        int a = -10;
+        int b = 7;
+        System.out.println(a+b>=0? "The amount is positive" : "The amount is negative");
     }
 	
     static void printColor() {
-	int value = 0;
-	if (value <=0) {
-	    System.out.println("Red");
-	} else if (value <=100) {
-	    System.out.println("Yellow");
-	} else {
-	    System.out.println("Green");
-	}
+        int value = 0;
+        if (value <=0) {
+            System.out.println("Red");
+        } else if (value <=100) {
+            System.out.println("Yellow");
+        } else {
+            System.out.println("Green");
+        }
     }
 	
     static void compareNumbers() {
-	int a = 11;
-	int b = 3;
-	System.out.println(a>=b? "a >= b" : "a < b");
+        int a = 11;
+        int b = 3;
+        System.out.println(a>=b? "a >= b" : "a < b");
     }
 }
