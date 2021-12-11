edc70ae383ba49086ee263941999911ccf227762
KaterinaTeri <95863911+KaterinaTeri@users.noreply.github.com>
2021-12-11 11:14:50 +0300

    Корректировка отступов/внешнего вида

--- PervoeDZ.java
+++ PervoeDZ.java
@@ -1,38 +1,37 @@
 class PervoeDZ {
     public static void main(String[] args) {
         printThreeWords();
-		checkSumSign();
-		printColor();
-		compareNumbers();
+	checkSumSign();
+	printColor();
+	compareNumbers();
     }
 	
-	static void printThreeWords() {
+    static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
 	
-   	static void checkSumSign() {
-		int a = -10;
-		int b = 7;
-		System.out.println(a+b>=0? "The amount is positive" : "The amount is negative");
-	}
+    static void checkSumSign() {
+	int a = -10;
+	int b = 7;
+	System.out.println(a+b>=0? "The amount is positive" : "The amount is negative");
+    }
 	
-	static void printColor() {
-		int value = 0;
-		if (value <=0) {
-			System.out.println("Red");
-		} else if (value <=100) {
-			System.out.println("Yellow");
-		} else {
-			System.out.println("Green");
-		}
-		
+    static void printColor() {
+	int value = 0;
+	if (value <=0) {
+	    System.out.println("Red");
+	} else if (value <=100) {
+	    System.out.println("Yellow");
+	} else {
+	    System.out.println("Green");
 	}
+    }
 	
-	static void compareNumbers() {
-		int a = 11;
-		int b = 3;
-		System.out.println(a>=b? "a >= b" : "a < b");
-	}
+    static void compareNumbers() {
+	int a = 11;
+	int b = 3;
+	System.out.println(a>=b? "a >= b" : "a < b");
+    }
 }
