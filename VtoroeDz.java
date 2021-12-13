8e475fb93d6bbf446bf96388fa7efe02b7b6aefc
KaterinaTeri <95863911+KaterinaTeri@users.noreply.github.com>
2021-12-13 11:18:29 +0300

    Update VtoroeDz.java

--- VtoroeDz.java
+++ VtoroeDz.java
@@ -2,7 +2,7 @@
 * Java 1/ Homework #2
 *
 * @author Ekaterina Dumskaya
-* @version 12.12. 23:30 - 13.12. 00:19
+* @version 12.12. 23:30 - 13.12. 00:19 - 13.12. 11:17
 *
 */
 class VtoroeDz {
@@ -10,25 +10,26 @@ class VtoroeDz {
         returnSum(5, 1);
         checkPositiveOrNegative(0);
         returnTrueOrFalse(-1);
-        printText(10);
+        printText("Hello", 5);
     }
     
     static boolean returnSum(int a, int b) {
         int sum = a + b;
-        return (sum >= 10 & sum <= 20? true : false);
+        return sum >= 10 & sum <= 20;
     }
-    
+
     static void checkPositiveOrNegative(int a) {
-        System.out.println (a >=0? "Positive" : "Negative");
+        System.out.println(a >= 0 ? "Positive" : "Negative");
     }
-    
+
     static boolean returnTrueOrFalse(int a) {
-        return (a >= 0? false : true);
+        return a >= 0;
     }
     
-    static void printText(int b) {
-        for (int n = 0; n < b; n++){
-            System.out.println ("Hello");
+    static void printText(String str, int a) {
+        for (int n = 0; n < a; n++){
+            System.out.println (str);
         }
     }
 }
+
